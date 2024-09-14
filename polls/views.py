cb04b8e4b4f91c77da0b2f29bc451168da537a5b
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-09-14 14:40:31 -0600

    Se agregaron estilos a la vista /polls/

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,10 @@
-from django.db.models import F
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
 from polls.models import Choice, Question
-
-
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
