384d47f678c681cad3118f5d40b40b31c18a1c76
Agustín Balcázar <agus.b.reyes@gmail.com>
2024-09-16 07:48:37 -0600

    Se modificaron los campos de edición e inserción de la vista de administrador para el formulario de Questions

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from polls.models import Question, Choice
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("General information", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice) 
